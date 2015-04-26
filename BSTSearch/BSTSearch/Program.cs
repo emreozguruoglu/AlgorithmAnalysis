3ebe7cec47b868936b4aa3aec9fd659ab6be365c
EMRE ÖZGÜRÜOĞLU <emreozguruoglu@gmail.com>
2015-04-26 21:01:22 +0300

    BST Search & Heap Sort
    Algortihm Analysis Homework

---
+++ BSTSearch/BSTSearch/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class BST
+    {
+        //BST node sınıfı yaratılıyor.
+        class Node
+        {
+            public int data;
+            public Node left;
+            public Node right;
+            public Node(int value)
+            {
+                this.data = value;
+                left = null;
+                right = null;
+            }
+        }
+        class BinarySearchTree
+        {
+            public Node root;
+            static int count;
+            public BinarySearchTree()
+            {
+                root = null;
+            }
+
+            // BST Node'u yaratma ve geri döndürme
+            public Node addNode(int data)
+            {
+                Node temp = new Node(data);
+                if (root == null)
+                    root = temp;
+                count++;
+                return temp;
+            }
+
+            public void insert(Node root, Node newNode)
+            {
+                while (root != null)
+                {
+                    if (newNode.data > root.data)
+                    {
+                        if (root.right == null)
+                        {
+                            root.right = newNode;
+                            break;
+                        }
+                        root = root.right;
+                    }
+                    else
+                    {
+                        if (root.left == null)
+                        {
+                            root.left = newNode;
+                            break;
+                        }
+                        root = root.left;
+                    }
+                }
+            }
+            
+            public void inorder(Node root)
+            {
+                if (root != null)
+                {
+                    inorder(root.left);
+                    Console.Write(root.data + " ");
+                    inorder(root.right);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            BinarySearchTree bst = new BinarySearchTree();
+           
+            // Node'ları ağaç yapımıza ekliyoruz.Random değerler alınmaktadır.
+            bst.insert(bst.root, bst.addNode(rnd.Next(100)));
+            bst.insert(bst.root, bst.addNode(rnd.Next(100)));
+            bst.insert(bst.root, bst.addNode(rnd.Next(100)));
+            bst.insert(bst.root, bst.addNode(rnd.Next(100)));
+            bst.insert(bst.root, bst.addNode(rnd.Next(100)));
+            bst.insert(bst.root, bst.addNode(rnd.Next(100)));
+            bst.insert(bst.root, bst.addNode(rnd.Next(100)));
+            bst.insert(bst.root, bst.addNode(rnd.Next(100)));
+            bst.insert(bst.root, bst.addNode(rnd.Next(100)));
+            bst.insert(bst.root, bst.addNode(rnd.Next(100)));
+
+            // Düzenli şekle getirme
+            bst.inorder(bst.root);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HeapSort/HeapSort/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapSort
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            
+            Random rnd = new Random();
+            int[] arr = new int[10]; // Dizi elemanlari arr dizi değişkenine eklenecektir.
+
+            for (int i = 0; i < arr.Length; i++) // 10 tane eleman random olarak oluşturuldu.
+            {
+                arr[i] = rnd.Next(100); // 0 ile 100 arasında random sayılar oluşturuluyor.
+            }
+
+            HeapSort hs = new HeapSort();
+            hs.PerformHeapSort(arr);
+            Console.ReadLine();
+        }
+    }
+
+    class HeapSort
+    {
+
+        public static int swapnubmer = 0;
+        private int heapSize;
+
+        private void BuildHeap(int[] arr)
+        {
+            heapSize = arr.Length - 1;
+            for (int i = heapSize / 2; i >= 0; i--)
+            {
+                Heapify(arr, i);
+            }
+        }
+
+        private void Swap(int[] arr, int x, int y) //Dizi elamanlari swap yapilir.
+        {
+            int temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
+        private void Heapify(int[] arr, int index) //Dizi elemanlari burada düzenlenir.
+        {
+            int left = 2 * index;
+            int right = 2 * index + 1;
+            int largest = index;
+
+            if (left <= heapSize && arr[left] > arr[index])
+            {
+                largest = left;
+            }
+
+            if (right <= heapSize && arr[right] > arr[largest])
+            {
+                largest = right;
+            }
+
+            if (largest != index)
+            {
+                Swap(arr, index, largest);
+                swapnubmer = swapnubmer + 1;
+                Heapify(arr, largest);
+            }
+        }
+
+        public void PerformHeapSort(int[] arr)
+        {
+            BuildHeap(arr);
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Swap(arr, 0, i);
+                heapSize--;
+                Heapify(arr, 0);
+            }
+            DisplayArray(arr);
+        }
+
+        private void DisplayArray(int[] arr) //Dizi ekrana yazdirilir.
+        {
+            for (int i = 0; i < arr.Length; i++)
+            { Console.Write("[{0}]", arr[i]); }
+            Console.WriteLine();
+
+            Console.Write("Dizide toplam {0} adet swap yapilmistir.", swapnubmer);
+        }
+    }
+}
