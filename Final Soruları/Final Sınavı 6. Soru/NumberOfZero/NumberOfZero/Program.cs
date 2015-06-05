38125b3bf2f4475eb1dd0976631ea3bcad3d444f
EMRE ÖZGÜRÜOĞLU <emreozguruoglu@gmail.com>
2015-06-05 14:11:50 +0300

    Final Soruları Eklendi
    Final Sınavının 5. , 6.  ve 8. Sorularının Cevapları Eklendi

---
+++ Final Soruları/Final Sınavı 6. Soru/NumberOfZero/NumberOfZero/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = { 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1 };
+
+            int gecici = 0;
+            int sayac = 0;
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                while (dizi[i] == 0)
+                {
+                    if (dizi[i] == dizi[i + 1])
+                    {
+                        sayac = sayac + 1;
+                        gecici = sayac;
+                        break;
+                    }
+                    else
+                    {
+                        sayac = 1;
+                        break;
+                    }
+                }
+            }
+
+            if (sayac > gecici)
+            {
+                Console.WriteLine("Dizi'nin en çok sifir takip eden serisi:{0} tanedir.", sayac);
+            }
+            else
+            {
+                Console.WriteLine("Dizi'nin en çok takip eden 0 serisi:{0} tanedir.", gecici);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Final Soruları/Final Sınavı 8. Soru/ArrayResize/ArrayResize/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayResize
+{
+    class Program
+    {
+        static int[] array;
+        static int kapasite, boyut;
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bir Sayi Giriniz:");
+            string input = Console.ReadLine();
+            int n = Convert.ToInt32(input);
+            Random rand = new Random();
+            kapasite = 4;
+            array = new int[kapasite];
+            for (boyut = 0; boyut < n; boyut++)
+            {
+                if (boyut > (kapasite - 1)) {
+                    yeniBoyut();
+                }
+                array[boyut] = rand.Next();
+            }
+            print();
+            Console.ReadLine();
+        }
+
+        static void yeniBoyut()
+        {
+            kapasite *= 2;
+            int[] gecici = new int[kapasite];
+            for (int i = 0; i < boyut; i++)
+            {
+                gecici[i] = array[i];
+            }
+            array = gecici;
+            Console.WriteLine("Yeni Boyut:{0}", kapasite);
+        }
+
+        static void print()
+        {
+            for (int i = boyut; i > 0; i--)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
